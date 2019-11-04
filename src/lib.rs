579be16c7becff12e0affe0a35075b2d3640b38b
richard <richard>
2019-11-04 00:17:14 -0800

    no_std

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,62 @@
-use std::sync::Arc;
+#![no_std]
+extern crate alloc;
+use alloc::boxed::Box;
+use alloc::sync::Arc;
+use alloc::vec::Vec;
 pub use spin::Mutex;
 
-pub struct Store<T,A> where T:Default+Reduceable<A>+Sync+Send,A:Sync+Send{
-    pub state:Arc<Mutex<T>>,
-    watchers:Arc<Mutex<Vec<Box<dyn Fn(Arc<Mutex<T>>)->()+Sync+Send>>>>,
-    phantom: std::marker::PhantomData<A>
+pub struct Store<T, A>
+where
+    T: Default + Reduceable<A> + Sync + Send,
+    A: Sync + Send,
+{
+    pub state: Arc<Mutex<T>>,
+    watchers: Arc<Mutex<Vec<Box<dyn Fn(Arc<Mutex<T>>) -> () + Sync + Send>>>>,
+    phantom: core::marker::PhantomData<A>,
 }
 
-impl<T,A> Store<T,A> where T:Default+Reduceable<A>+Sync+Send,A:Sync+Send{
-    pub fn get() -> &'static Mutex<Self>{
+impl<T, A> Store<T, A>
+where
+    T: Default + Reduceable<A> + Sync + Send,
+    A: Sync + Send,
+{
+    pub fn get() -> &'static Mutex<Self> {
         globals::get()
     }
-    
-    pub fn reduce(&self,a:&A) -> Arc<Mutex<T>>{
-        let s = T::reduce(self.state.clone(),a);
+
+    pub fn reduce(&self, a: &A) -> Arc<Mutex<T>> {
+        let s = T::reduce(self.state.clone(), a);
         for w in self.watchers.lock().iter() {
             w(s.clone())
         }
         s
     }
 
-    pub fn watch<F>(&mut self, watcher:F) where F:'static+Fn(Arc<Mutex<T>>)->()+Sync+Send{
+    pub fn watch<F>(&mut self, watcher: F)
+    where
+        F: 'static + Fn(Arc<Mutex<T>>) -> () + Sync + Send,
+    {
         self.watchers.lock().push(Box::new(watcher))
     }
 }
 
-impl<T,A> Default for  Store<T,A> where T:Default+Reduceable<A>+Sync+Send,A:Sync+Send{
+impl<T, A> Default for Store<T, A>
+where
+    T: Default + Reduceable<A> + Sync + Send,
+    A: Sync + Send,
+{
     fn default() -> Self {
-        Store::<T,A>{
-            state:Arc::new(Mutex::new(T::default())),
+        Store::<T, A> {
+            state: Arc::new(Mutex::new(T::default())),
             watchers: Arc::new(Mutex::new(Vec::new())),
-            phantom: std::marker::PhantomData
+            phantom: core::marker::PhantomData,
         }
     }
 }
 
-pub trait Reduceable<A> where A:Sync+Send {
-    fn reduce(state:Arc<Mutex<Self>>,action:&A) -> Arc<Mutex<Self>>;
+pub trait Reduceable<A>
+where
+    A: Sync + Send,
+{
+    fn reduce(state: Arc<Mutex<Self>>, action: &A) -> Arc<Mutex<Self>>;
 }
